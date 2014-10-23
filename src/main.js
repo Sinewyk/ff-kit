0894752abb1dad89b05a66ac49767bc710b9ac72
Sinewyk <sinewyk@gmail.com>
2014-10-23 23:10:09 +0200

    trying jed ...

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 //languageConf @todo: read conf
 var locale = 'fr';
-var t = require('_i18n');
-t.textdomain(locale);
-t.moment.locale(locale);
+require('_i18n').moment.locale(locale);
 
 if (process.env.NODE_ENV === 'development') {
     require('bluebird').longStackTraces();
